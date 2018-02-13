9913102f62af0b31c3599bdb500cebc99763c16b
Dave Laptop <ian.d.hann@gmail.com>
2018-02-13 15:44:01 -0500

    Assignment 4 work
    Initial work in main, data validation

---
+++ Assignment4/Assignment4/Assignment4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Assignment4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32 shapeSelection = GetMenuInput();
+            
+
+
+            Console.Write("The value entered is " + shapeSelection);
+            Console.Read();
+
+        }
+
+        private static Int32 GetMenuInput()
+        {
+            bool repeat = true; // Set flag for error handling
+            Int32 output = 0; // Return value
+
+            while (repeat) // While loop to take input, validate, and repeat if incorrect
+            {
+                Console.Clear(); // Keep the screen tidy
+
+                // Display menu and receive input
+                Console.Write("***AREA CALCULATION***" + "\n" +
+                              "1. Circle" + "\n" +
+                              "2. Rectangle" + "\n" +
+                              "3. Cylinder" + "\n\n" +
+
+                              "Please select the shape you would like to calculate the area for(1, 2 or 3):");
+
+                string menuChoice = Console.ReadLine();
+
+                try
+                {
+                    output = Convert.ToInt32(menuChoice);
+                }// Ent Try
+                catch (FormatException)
+                {
+                    Console.WriteLine("\n\nThe value entered is not a number. ");
+                }// End Catch
+                catch (OverflowException)
+                {
+                    Console.WriteLine("\n\nThe number cannot fit in an Int32. ");
+                }// End Catch 2
+                finally
+                {
+                    if (output > 3 || output < 1)
+                    {
+                        Console.WriteLine("\n\nValue entered is not one of the available options. ");
+                    } // End If
+                }// End Finally
+
+                Console.Write("\nYou have entered " + menuChoice + ". Try again? Enter Y/N: ");
+                string loopResponse = Console.ReadLine();
+
+                if (loopResponse == "Y" || loopResponse == "y")
+                {
+                    repeat = true;
+                }// End If
+                else
+                {
+                    repeat = false;
+                }// End Else
+
+            }// End While
+
+            return output;
+            
+        }
+
+       
+
+    }
+}
---
+++ Class 4 Exercises/Class4Exercises/Class4Exercises/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class4Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age = 99;
+
+            if (age < 16)
+            {
+                Console.WriteLine("You can't drive!");
+            }
+
+            else if (age > 15 && age < 18)
+            {
+                Console.WriteLine("You can drive but not vote");
+            }
+
+            else if (age > 17 && age < 25)
+            {
+                Console.WriteLine("You can vote but not rent a car");
+            }
+
+            else if (age == 40)
+            {
+                Console.WriteLine("You're over the hill!");
+            }
+
+            else if (age > 79)
+            {
+                Console.WriteLine("You can't eat solid food anymore!");
+            }
+
+            else
+            {
+                Console.WriteLine("You can do whatever you like");
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.Read();
+
+        }
+
+        public getAge(int age)
+        {
+            
+        }
+
+    }
+}

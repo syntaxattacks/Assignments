ceae63c8c850c4e4c69a9e9cf982c1094f15bd68
Dave Laptop <ian.d.hann@gmail.com>
2018-02-13 20:14:45 -0500

    Updated error handling
    Move the last statements to the finally area, nested an if statement in finally that checked to make sure an integer was entered before displaying error data regarding integer values

--- Assignment4/Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Assignment4/Program.cs
@@ -1,27 +1,21 @@
 ﻿using System;
 
-namespace Assignment4
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace Assignment4{
+    class Program{
+        public static void Main(string[] args) {
             Int32 shapeSelection = GetMenuInput();
             
-
 
             Console.Write("The value entered is " + shapeSelection);
             Console.Read();
 
         }
 
-        private static Int32 GetMenuInput()
-        {
+        private static Int32 GetMenuInput() {
             bool repeat = true; // Set flag for error handling
             Int32 output = 0; // Return value
 
-            while (repeat) // While loop to take input, validate, and repeat if incorrect
-            {
+            while (repeat) { // While loop to take input, validate, and repeat if it not what is expected
                 Console.Clear(); // Keep the screen tidy
 
                 // Display menu and receive input
@@ -31,41 +25,31 @@ namespace Assignment4
                               "3. Cylinder" + "\n\n" +
 
                               "Please select the shape you would like to calculate the area for(1, 2 or 3):");
-
                 string menuChoice = Console.ReadLine();
 
-                try
-                {
-                    output = Convert.ToInt32(menuChoice);
-                }// Ent Try
-                catch (FormatException)
-                {
-                    Console.WriteLine("\n\nThe value entered is not a number. ");
+                /*** ERROR HANDLING ***/
+                try { output = Convert.ToInt32(menuChoice); }// End Try
+                catch (FormatException) {
+                    Console.WriteLine("\n\nThe value entered is not a number. Press any key to continue...");
+                    Console.ReadLine();
                 }// End Catch
-                catch (OverflowException)
-                {
-                    Console.WriteLine("\n\nThe number cannot fit in an Int32. ");
+                catch (OverflowException) {
+                    Console.WriteLine("\n\nThe number cannot fit in an Int32. Press any key to continue...");
+                    Console.ReadLine();
                 }// End Catch 2
                 finally
                 {
-                    if (output > 3 || output < 1)
-                    {
-                        Console.WriteLine("\n\nValue entered is not one of the available options. ");
-                    } // End If
+                    Int32 testParse; // To be used in initial if statement
+                    if(Int32.TryParse(menuChoice, out testParse)) { // I had to nest the if statements, otherwise the writeline would display in addition to any errors caught
+                        if (output > 0 && output < 4){
+                            repeat = false;
+                        } // End Nested If
+                        else if (output > 3 || output < 1) {
+                            Console.WriteLine("\n\nThe value entered is not a valid option. Press any key to continue...");
+                            Console.ReadLine();
+                        }// End Else If
+                    }// End If
                 }// End Finally
-
-                Console.Write("\nYou have entered " + menuChoice + ". Try again? Enter Y/N: ");
-                string loopResponse = Console.ReadLine();
-
-                if (loopResponse == "Y" || loopResponse == "y")
-                {
-                    repeat = true;
-                }// End If
-                else
-                {
-                    repeat = false;
-                }// End Else
-
             }// End While
 
             return output;

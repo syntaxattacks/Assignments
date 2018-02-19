248732b286bf524f147057370c920adec84c59e9
Dave Laptop <ian.d.hann@gmail.com>
2018-02-18 20:38:02 -0500

    Added comments

--- Assignment4/Assignment4/Assignment4/Circle.cs
+++ Assignment4/Assignment4/Assignment4/Circle.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*  
+    Name: Ian Hann
+    Date: 2/18/2018
+    Assignment # 4
+    Chapter #: 5    Page #:323  Problem #:6
+    
+    Circle.cs -- Write an application that computes the area of a circle, rectangle, and cylinder.  
+    Display a menu showing the three options.  Allow users to input which figure they want to see calculated.  
+    Based on the value inputted, prompt for appropriate dimensionis and perform the calculations.
+*/
+
+using System;
 
 namespace Assignment4 {
     class Circle {
--- Assignment4/Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Assignment4/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*  
+    Name: Ian Hann
+    Date: 2/18/2018
+    Assignment # 4
+    Chapter #: 5    Page #:323  Problem #:6
+    
+    Program.cs -- Write an application that computes the area of a circle, rectangle, and cylinder.  
+    Display a menu showing the three options.  Allow users to input which figure they want to see calculated.  
+    Based on the value inputted, prompt for appropriate dimensionis and perform the calculations.
+*/
+
+using System;
 
 namespace Assignment4 {
     class Program{
